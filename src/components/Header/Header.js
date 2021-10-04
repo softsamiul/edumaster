2f568dacda19567b74c0c3d9c4ce056882e8dfa7
softsamiul <softsamiul@gmail.com>
2021-10-04 19:51:17 +0530

    header sticky featured added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ import Navbar from '../Navbar/Navbar';
 const Header = () => {
     const element = <FontAwesomeIcon style={{marginRight:'15px'}} icon={faUserGraduate} />
     return (
-        <header className="bg-blue-800 py-4 text-white">
+        <header className="bg-blue-800 py-4 text-white sticky top-0">
             <div className="w-4/5 mx-auto flex-none md:flex justify-between items-center">
                 <div className="brand">
                 <NavLink className="text-4xl font-bold" to="/home">{element}EDUMASTER</NavLink>
