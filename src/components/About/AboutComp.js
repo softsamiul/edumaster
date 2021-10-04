123fadecf733443f900e5407e75042265b516098
softsamiul <softsamiul@gmail.com>
2021-10-04 11:57:59 +0530

    about modified

--- src/components/About/AboutComp.js
+++ src/components/About/AboutComp.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import aboutImg from '../../images/about-us.png';
+import accunture from '../../images/company-logo/accenture.png';
+import cognizint from '../../images/company-logo/cognizint.png';
+import flipkart from '../../images/company-logo/flipkart.png';
+import infosys from '../../images/company-logo/infosys.png';
+import mahindra from '../../images/company-logo/mahindra.webp';
+import tata from '../../images/company-logo/tata.png';
+import wipro from '../../images/company-logo/wipro.png';
 import './AboutComp.css';
 const AboutComp = () => {
     return (
@@ -11,6 +18,32 @@ const AboutComp = () => {
                 <div className="w-2/4">
                     <h2 className="text-4xl font-bold mb-2">Achieve Your Goals</h2>
                     <p className="text-basic">We are a modern, global school for a modern, global academic experience. We offer world's top quality education. Learning isn't just limited to classes with our practice section, mock tests and lecture notes shared as PDFs for your revision. Talk with instructors, ask questions, answer live polls, and get your doubts cleared - all while the class is going on.</p>
+                </div>
+            </div>
+            <div className="py-12">
+                <h2 className="text-4xl font-bold text-center mb-10">Trusted Company</h2>
+                <div className="w-3/4 mx-auto grid grid-cols-7 gap-2 h-24">
+                    <div>
+                        <img className="h-16 rounded-full" src={wipro} alt="" />
+                    </div>
+                    <div>
+                        <img className="h-16 rounded-full" src={accunture} alt="" />
+                    </div>
+                    <div>
+                        <img className="h-16 rounded-full" src={tata} alt="" />
+                    </div>
+                    <div>
+                        <img className="h-16 rounded-full" src={infosys} alt="" />
+                    </div>
+                    <div>
+                        <img className="h-16 rounded-full" src={cognizint} alt="" />
+                    </div>
+                    <div>
+                        <img className="h-16 rounded-full" src={flipkart} alt="" />
+                    </div>
+                    <div>
+                        <img className="h-16 rounded-full" src={mahindra} alt="" />
+                    </div>
                 </div>
             </div>
         </div>
